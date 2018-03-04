72f5bac798a3288b7662c78501606c39725dbd06
Ashish Ratnawat <ashishratnawat452@gmail.com>
2018-03-04 17:39:16 +0530

    lane detection, edge detection in gta 5
    lane detection, edge detection in gta 5

---
+++ lane detection/lanedetection.py
@@ -0,0 +1,49 @@
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import time
+from directkeys import ReleaseKey, PressKey, W, A, S, D 
+
+
+def draw_lines(img,lines):
+    for line in lines:
+        coords = line[0]
+        cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
+
+
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500],
+                         ], np.int32)
+    processed_img = roi(processed_img, [vertices])
+
+    # more info: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
+    #                          edges       rho   theta   thresh         # min length, max gap:        
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180,      20,         15)
+    draw_lines(processed_img,lines)
+    return processed_img
+
+
+def roi(img, vertices):
+    #blank mask:
+    mask = np.zeros_like(img)
+    # fill the mask
+    cv2.fillPoly(mask, vertices, 255)
+    # now only show the area that is the mask
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
+
+def main():
+    last_time = time.time()
+    while(True):
+        screen =  np.array(ImageGrab.grab(bbox=(0,40, 800, 640)))
+        new_screen = process_img(screen)
+        print('Loop took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+        cv2.imshow('window', new_screen)
+        #cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
